82a107a9d2953b5b34760f3c31d5840f25340ed3
AndrejSlavik <xslavika@stuba.sk>
2021-03-17 14:18:00 +0100

    Player name's initialization

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -2,6 +2,12 @@ package sk.stuba.fei.uim.oop;
 
 public class Assignment1 {
     public static void main(String[] args) {
-
+        System.out.println("Enter number of players: ");
+        int playerCount = KeyboardInput.readInt();
+        Players player = new Players();
+        for (int i = 0 ; i<playerCount; i++) {
+            System.out.println("Enter name of the " + i + ". player:");
+            player.name = KeyboardInput.readString();
+        }
     }
 }
